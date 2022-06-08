32b1c6578afd7ca674a0461123bc93d1e7bac8d0
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-06-08 14:37:37 +0300

    Modified S9Hw1

--- S9Hw1_RecursionFromMtoN/Program.cs
+++ S9Hw1_RecursionFromMtoN/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Показать натуральные числа от M до N, N и M заданы
-void PrintNumber(int min, int max)
+void RecPrintNumbersFromMinToMax(int min, int max)
 {
     if (max < min)
     {
@@ -7,17 +7,19 @@ void PrintNumber(int min, int max)
         return;
     }
     Console.Write(min + " ");
-    PrintNumber(min + 1, max);
+    RecPrintNumbersFromMinToMax(min + 1, max);
 }
 Console.Clear();
 Console.Write("Введите число M: ");
 int m = int.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите число N: ");
 int n = int.Parse(Console.ReadLine() ?? "0");
-if (m > n)
+if (m <= 0 || n <= 0)
+    Console.WriteLine("Введены не натуральные числа!");
+else if (m > n)
 {
     Console.WriteLine("Число M больше числа N, меняем их местами");
     int tmp = m; m = n; n = tmp;
+    RecPrintNumbersFromMinToMax(m, n);
 }
-Console.WriteLine("Диапазон чисел:");
-PrintNumber(m, n);
+else RecPrintNumbersFromMinToMax(m, n);
