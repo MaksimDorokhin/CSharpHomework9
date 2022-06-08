24c321b32794e1bce54a1cfe74ae35bdb8b35c3f
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-06-08 14:22:53 +0300

    Added S9Hw1 task: recursion from M to N

---
+++ S9Hw1_RecursionFromMtoN/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void PrintNumber(int min, int max)
+{
+    if (max < min)
+    {
+        Console.WriteLine();
+        return;
+    }
+    Console.Write(min + " ");
+    PrintNumber(min + 1, max);
+}
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+if (m > n)
+{
+    Console.WriteLine("Число M больше числа N, меняем их местами");
+    int tmp = m; m = n; n = tmp;
+}
+Console.WriteLine("Диапазон чисел:");
+PrintNumber(m, n);
